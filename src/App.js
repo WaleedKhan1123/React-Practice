fdd65a554441e8db995937e7a3f7555b4680153e
waleed-khan11 <115110572+waleed-khan11@users.noreply.github.com>
2024-06-15 21:54:11 +0500

    Brief description of your changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import "./styles/style.css";
 
-import './App.css';
-
+function MyButton() {
+  return (
+    <button>
+      I'm a button
+    </button>
+  );
+}
 function App() {
   return (
     <div className="App">
     
        
-        <h1>
-          Welcome to react practice 
+        <h1 className="header">
+          Welcome to react practice
+          
         </h1>
+        <MyButton/>
       
     </div>
   );
---
+++ src/Component/Button.js
@@ -0,0 +1,8 @@
+
+function myButton(){
+    return(<button>
+        Im a button
+    </button>)
+}
+
+export default myButton
