e073b36301fc68bc4e10d7f5bf604162b2b5a93c
waleed-khan11 <115110572+waleed-khan11@users.noreply.github.com>
2024-06-17 12:11:05 +0500

    react concepts 3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import "./styles/style.css";
+import { useState } from "react";
 
-function MyButton() {
-  return (
-    <button>
-      I'm a button
-    </button>
-  );
-}
-
-const User= {
- name: 'Waleed',
- reg: 'SP21-BSE-006'
-}
-
-const name={
-  name3: 'Hedy Lamarr',
-  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
-  imageSize: 90,
-} 
 function App() {
+
+  
+  function MyButton() {
+    const [counter, setCounter]= useState(0);
+    function clicked (){
+      setCounter( counter+1); 
+     }
+    return (
+     
+      <button onClick={clicked}>
+        
+        I'm a button
+        {counter}
+      </button>
+    );
+  }
+ 
+  const User= {
+   name: 'Waleed',
+   reg: 'SP21-BSE-006'
+  }
+  
+  const name={
+    name3: 'Hedy Lamarr',
+    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+    imageSize: 90,
+  } 
+  
+  const products= [
+    {title:'onion', id: 1},
+    {title:'fruit', id: 2},
+    {title:'Grapes', id: 3},
+    {title:'mangoes', id: 4},
+  ]
+  
+
+  
+
   return (
     <div className="App">
     
@@ -27,12 +48,21 @@ function App() {
           Welcome to react practice
           
         </h1>
-        <MyButton/>
+        <MyButton  />
+        <MyButton  />
        <h1>{User.name}</h1>
        <h1>{User.reg}</h1>
 
        <img className="avatar"
        src={name.imageUrl} alt={'photo of user '+name.name3} style={{width:name.imageSize, height:name.imageSize}}/>
+       <ul>
+
+        {products.map(product => 
+          <li key={product.id}>
+            {product.title}
+          </li>
+        )}
+       </ul>
        
     </div>
   );
