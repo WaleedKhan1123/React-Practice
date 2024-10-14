035e279f043a7840b9d215b4ade5447424522745
waleed-khan11 <waleed.kn270@gmail.com>
2024-10-15 00:06:22 +0500

    React-practice

--- src/Component/Gallery.js
+++ src/Component/Gallery.js
@@ -3,7 +3,7 @@ import Profile from "./Profile";
   export default function Gallery() {
     return (
       <section>
-        <h1>Amazing scientists</h1>
+        <h1>Amazing scientistss</h1>
         <Profile />
         <Profile />
         <Profile />
