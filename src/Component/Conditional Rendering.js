2708c927b71fbddbae4a9cd59821398641e126c3
waleed-khan11 <waleed.kn270@gmail.com>
2024-10-15 19:01:34 +0500

    React-Practice

--- src/Component/Conditional Rendering.js
+++ src/Component/Conditional Rendering.js
@@ -1,32 +1,78 @@
-function Item({ name, importance }) {
-    return (
-      <li className="item">
-        {name} {importance >0 && <i>(importance: {importance}) </i>}
-      </li>
-    );
-  }
-  
-  export default function PackingList() {
+
+function Drink({ name }) {
+    let plant,Caffeine,age;
+    if(name==='tea'){
+
+        plant='leaf';
+        Caffeine = '15–70 mg/cup';
+        age = '4,000+ years'
+    }
+
+    else{
+        plant='bean';
+        Caffeine = '80–185 mg/cup';
+        age = '1,000+ years'
+
+    }
     return (
       <section>
-        <h1>Sally Ride's Packing List</h1>
-        <ul>
-          <Item 
-            importance={9} 
-            name="Space suit" 
-          />
-          <Item 
-            importance={0} 
-            name="Helmet with a golden leaf" 
-          />
-          <Item 
-            importance={6} 
-            name="Photo of Tam" 
-          />
-        </ul>
+        <h1>{name}</h1>
+        <dl>
+          
+          <dt>Part of plant</dt>
+          <dd>{plant}</dd>
+          <dt>Caffeine content</dt>
+          <dd>{Caffeine}</dd>
+          <dt>Age</dt>
+          <dd>{age}</dd>
+        </dl>
       </section>
     );
   }
+  
+  export default function DrinkList() {
+    return (
+      <div>
+        <Drink name="tea" />
+        <Drink name="coffee" />
+      </div>
+    );
+  }
+  
+
+
+
+
+
+// function Item({ name, importance }) {
+//     return (
+//       <li className="item">
+//         {name} {importance >0 && <i>(importance: {importance}) </i>}
+//       </li>
+//     );
+//   }
+  
+//   export default function PackingList() {
+//     return (
+//       <section>
+//         <h1>Sally Ride's Packing List</h1>
+//         <ul>
+//           <Item 
+//             importance={9} 
+//             name="Space suit" 
+//           />
+//           <Item 
+//             importance={0} 
+//             name="Helmet with a golden leaf" 
+//           />
+//           <Item 
+//             importance={6} 
+//             name="Photo of Tam" 
+//           />
+//         </ul>
+//       </section>
+//     );
+//   }
   
 
 
