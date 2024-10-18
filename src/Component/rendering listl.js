452168a2c3c31613f2c7dbd0be3af780877773f6
waleed-khan11 <waleed.kn270@gmail.com>
2024-10-18 23:45:02 +0500

    React Practice

--- src/Component/rendering listl.js
+++ src/Component/rendering listl.js
@@ -7,13 +7,25 @@ const poem = {
 };
 
 export default function Poem() {
+  let output = [];
+
+  // Fill the output array
+  poem.lines.forEach((line, i) => {
+    output.push(
+      <hr key={i + '-separator'} />
+    );
+    output.push(
+      <p key={i + '-text'}>
+        {line}
+      </p>
+    );
+  });
+  // Remove the first <hr />
+  output.shift();
+
   return (
     <article>
-      {poem.lines.map((line, index) =>
-        <p key={index}>
-          {line}
-        </p>
-      )}
+      {output}
     </article>
   );
 }
