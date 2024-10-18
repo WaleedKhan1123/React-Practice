a9d1cc9256ca97602713855b2d0f897005af2185
waleed-khan11 <waleed.kn270@gmail.com>
2024-10-18 22:44:11 +0500

    React Practice

--- src/Component/rendering listl.js
+++ src/Component/rendering listl.js
@@ -1,32 +1,120 @@
-const people = [{
-id: 0,
-name : 'Creola Katherine Johnson: mathematician'
-},
-{
-id : 1,
-name : 'Mario José Molina-Pasquel Henríquez: chemist',
-},
-{
-id : 2,
-name: 'Mohammad Abdus Salam: physicist',
-},
-{
-id : 3,
-name : 'Percy Lavon Julian: chemist',
+ const people = [{
+  id: 0,
+  name: 'Creola Katherine Johnson',
+  profession: 'mathematician',
+  accomplishment: 'spaceflight calculations',
+  imageId: 'MK3eW3A'
+}, {
+  id: 1,
+  name: 'Mario José Molina-Pasquel Henríquez',
+  profession: 'chemist',
+  accomplishment: 'discovery of Arctic ozone hole',
+  imageId: 'mynHUSa'
+}, {
+  id: 2,
+  name: 'Mohammad Abdus Salam',
+  profession: 'physicist',
+  accomplishment: 'electromagnetism theory',
+  imageId: 'bE7W1ji'
+}, {
+  id: 3,
+  name: 'Percy Lavon Julian',
+  profession: 'chemist',
+  accomplishment: 'pioneering cortisone drugs, steroids and birth control pills',
+  imageId: 'IOjWm71'
+}, {
+  id: 4,
+  name: 'Subrahmanyan Chandrasekhar',
+  profession: 'astrophysicist',
+  accomplishment: 'white dwarf star mass calculations',
+  imageId: 'lrWQx8l'
+}];
+
+function getImageUrl(person) {
+  return (
+    'https://i.imgur.com/' +
+    person.imageId +
+    's.jpg'
+  );
 }
+
+export default function List() {
+  const chemist  = people.filter(people => people.profession==='chemist')
+  const other  = people.filter(people => people.profession!=='chemist')
+  const listchemist = chemist.map(person =>
+    <li key={person.id}>
+      <img
+        src={getImageUrl(person)}
+        alt={person.name}
+      />
+      <p>
+        <b>{person.name}:</b>
+        {' ' + person.profession + ' '}
+        known for {person.accomplishment}
+      </p>
+    </li>
+  );
+  const everyOne = other.map(person =>
+    <li key={person.id}>
+      <img
+        src={getImageUrl(person)}
+        alt={person.name}
+      />
+      <p>
+        <b>{person.name}:</b>
+        {' ' + person.profession + ' '}
+        known for {person.accomplishment}
+      </p>
+    </li>
+  );
+  return (
+    <article>
+      <h1>Chemist</h1>
+      <ul>{listchemist}</ul>
+      <h1>EveryOne Else</h1>
+      <ul>{everyOne}</ul>
+    </article>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+// const people = [{
+// id: 0,
+// name : 'Creola Katherine Johnson: mathematician'
+// },
+// {
+// id : 1,
+// name : 'Mario José Molina-Pasquel Henríquez: chemist',
+// },
+// {
+// id : 2,
+// name: 'Mohammad Abdus Salam: physicist',
+// },
+// {
+// id : 3,
+// name : 'Percy Lavon Julian: chemist',
+// }
     
 
-  ];
+//   ];
 
   
   
-function List() {
-    const listItems = people.map(person => 
-    <li key={person.id}>{person.name}</li>    
-    )
-    return ( <>
-    <h1>{listItems}</h1>
-    </> );
-}
+// function List() {
+//     const listItems = people.map(person => 
+//     <li key={person.id}>{person.name}</li>    
+//     )
+//     return ( <>
+//     <h1>{listItems}</h1>
+//     </> );
+// }
 
-export default List;
+// export default List;
