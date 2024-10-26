63f3958496c4d75285a7b28790015ef6712e4a2b
waleed-khan11 <waleed.kn270@gmail.com>
2024-10-26 22:19:27 +0500

    react Changes

--- src/Component/Responding to Events.js
+++ src/Component/Responding to Events.js
@@ -1,11 +1,22 @@
 function ResToEvents() {
-    function handleclick() {
-        console.log("function called")
-    }
+    // Regular function
+
+  // Arrow function
+  const greet =function greet(name) {
+    return `Hello, ${name}`;
+  }
+   
+  
+  
+//   (name) => `Hello, ${name}`;
+  
     return (<div>
-        <button onClick={handleclick}>
+        <button onClick={function handleclick() {
+        console.log("function called")
+    }}>
       I don't do anything
     </button>
+    {greet}
     </div>  );
 }
 
