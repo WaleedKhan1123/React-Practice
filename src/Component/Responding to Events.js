22a2bacaf3d806f6f59728828d89ecddebab75d2
waleed-khan11 <waleed.kn270@gmail.com>
2024-10-31 21:43:06 +0500

    React Practice

--- src/Component/Responding to Events.js
+++ src/Component/Responding to Events.js
@@ -1,39 +1,61 @@
-function Button({ onClick, children }) {
+function Button({ onSmash, children }) {
   return (
-    <button onClick={onClick}>
+    <button onClick={onSmash}>
       {children}
     </button>
-  );
-}
-
-function PlayButton({ movieName }) {
-  function handlePlayClick() {
-    alert(`Playing ${movieName}!`);
-  }
-
-  return (
-    <Button onClick={handlePlayClick}>
-      Play "{movieName}"
-    </Button>
-  );
-}
-
-function UploadButton() {
-  return (
-    <Button onClick={() => alert('Uploading!')}>
-      Upload Image
-    </Button>
   );
 }
 
 export default function Toolbar() {
   return (
     <div>
-      <PlayButton movieName="Kiki's Delivery Service" />
-      <UploadButton />
+      <Button onSmash={() => alert('Playing!')}>
+        Play Movie
+      </Button>
+      <Button onSmash={() => alert('Uploading!')}>
+        Upload Image
+      </Button>
     </div>
   );
 }
+
+
+// function Button({ onClick, children }) {
+//   return (
+//     <button onClick={onClick}>
+//       {children}
+//     </button>
+//   );
+// }
+
+// function PlayButton({ movieName }) {
+//   function handlePlayClick() {
+//     alert(`Playing ${movieName}!`);
+//   }
+
+//   return (
+//     <Button onClick={handlePlayClick}>
+//       Play "{movieName}"
+//     </Button>
+//   );
+// }
+
+// function UploadButton() {
+//   return (
+//     <Button onClick={() => alert('Uploading!')}>
+//       Upload Image
+//     </Button>
+//   );
+// }
+
+// export default function Toolbar() {
+//   return (
+//     <div>
+//       <PlayButton movieName="Kiki's Delivery Service" />
+//       <UploadButton />
+//     </div>
+//   );
+// }
 
 
 
