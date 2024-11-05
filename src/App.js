bf2dfdea33323d6f39bd573fdf00060778a76021
waleed-khan11 <waleed.kn270@gmail.com>
2024-11-06 01:43:58 +0500

    React Practice

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ import "./styles/style.css";
 // import Clock from "./Component/keeping components pure";
 // import ResToEvents from "./Component/Responding to Events";
 // import Toolbar from "./Component/Responding to Events";
-// import LightSwitch from "./Component/Responding to Events";
-import ColorSwitch from "./Component/Responding to Events";
+import Gallery from "./Component/State A Component's Memory";
+
 export default function App(){
 
   return(<>
-  <ColorSwitch/>
+  <Gallery/>  
     </>)
 }
 
