4ae25d9fb36fe8a1f631cb55d76c37fd24f7b9ed
waleed-khan11 <waleed.kn270@gmail.com>
2024-11-06 22:13:49 +0500

    React Practice

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ import "./styles/style.css";
 // import Clock from "./Component/keeping components pure";
 // import ResToEvents from "./Component/Responding to Events";
 // import Toolbar from "./Component/Responding to Events";
-import Form from "./Component/State A Component's Memory";
+import FeedbackForm from "./Component/State A Component's Memory";
 export default function App(){
 
   return(<>
-  <Form/>  
+  <FeedbackForm/>  
     </>)
 }
 
